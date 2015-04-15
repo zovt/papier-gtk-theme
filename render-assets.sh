5f567f53e6e327a80ef2ce4c188c27a1fb07bcb2
zovt <zovtly@gmail.com>
2015-04-15 15:15:53 -0700

    Whoops

--- render-assets.sh
+++ render-assets.sh
@@ -5,10 +5,10 @@ echo "Rendering GTK3 Assets"
 ./render-gtk3-assets.py
 
 echo "Rendering HiDPI GTK3 Assets"
-./render-gtk3-assets-hidpi.py.py
+./render-gtk3-assets-hidpi.py
 
 echo "Rendering GNOME Shell Assets"
-./render-gnome-shell-assets.py.py
+./render-gnome-shell-assets.py
 
 echo "Rendering WM Assets"
 ./render-wm-assets.py
